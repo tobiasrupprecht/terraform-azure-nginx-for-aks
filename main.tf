fa974aaa3feba6930fac224a9b3e8c20ce324c49
tobiasrupprecht <trupprecht@hashicorp.com>
2022-01-25 14:53:50 +0100

    added output and LoadBalancer

--- main.tf
+++ main.tf
@@ -86,3 +86,20 @@ resource "kubernetes_deployment" "nginx" {
     }
   }
 }
+
+resource "kubernetes_service" "nginx" {
+  metadata {
+    name = "nginx-example"
+  }
+  spec {
+    selector = {
+      App = kubernetes_deployment.nginx.spec.0.template.0.metadata[0].labels.App
+    }
+    port {
+      port        = 80
+      target_port = 80
+    }
+
+    type = "LoadBalancer"
+  }
+}
